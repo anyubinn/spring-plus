dbf1fd8033d2639432948058fa32a98ddd29936d
anyubinn <anyubin0724@naver.com>
2025-05-01 16:04:28 +0900

    Feat: Lv1-3. 코드 개선 퀴즈 - 할 일 검색 시 weather 조건으로 검색 가능 - 할 일 검색 시 수정일 시작, 끝 기준으로 검색 가능 - 검색 조건은 없어도 가능 - 기존 Fetch Join -> EntityGraph로 변경

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -1,8 +1,11 @@
 package org.example.expert.domain.todo.repository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import org.example.expert.domain.todo.entity.Todo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,8 +14,9 @@ import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+    @EntityGraph(attributePaths = "user")
+    @Query("SELECT t FROM Todo t WHERE (:weather IS NULL OR t.weather = :weather) AND (:startDate IS NULL OR t.modifiedAt >= :startDate) AND (:endDate IS NULL OR t.modifiedAt <= :endDate) ORDER BY t.modifiedAt DESC")
+    Page<Todo> findAllByOrderByModifiedAtDesc(@Param("weather") String weather, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, Pageable pageable);
 
     @Query("SELECT t FROM Todo t " +
             "LEFT JOIN t.user " +

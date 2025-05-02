955cca2f31c151a0a72b64f8fa1ed425bf4b6d1d
anyubinn <anyubin0724@naver.com>
2025-05-02 14:59:15 +0900

    Feat: Lv3-1. QueryDSL을 사용하여 검색 기능 만들기 - 검색 API 추가 - todo 제목, 매니저 닉네임 기준으로 검색 가능 - 검색 결과로 todo 제목, 담당자 수, 댓글 수가 나옴 - 검색 결과는 페이징 처리되어 반환됨

--- src/main/java/org/example/expert/domain/todo/controller/TodoController.java
+++ src/main/java/org/example/expert/domain/todo/controller/TodoController.java
@@ -7,6 +7,7 @@ import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.todo.dto.request.TodoSaveRequest;
 import org.example.expert.domain.todo.dto.response.TodoResponse;
 import org.example.expert.domain.todo.dto.response.TodoSaveResponse;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.service.TodoService;
 import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -38,6 +39,18 @@ public class TodoController {
             @RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime endDate
             ) {
         return ResponseEntity.ok(todoService.getTodos(page, size, weather, startDate, endDate));
+    }
+
+    @GetMapping("/todos/search")
+    public ResponseEntity<Page<TodoSearchResponse>> searchTodos(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String title,
+            @RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime startDate,
+            @RequestParam(required = false) @DateTimeFormat(iso = ISO.DATE_TIME) LocalDateTime endDate,
+            @RequestParam(required = false) String nickname
+    ) {
+        return ResponseEntity.ok(todoService.searchTodos(page, size, title, startDate, endDate, nickname));
     }
 
     @GetMapping("/todos/{todoId}")
--- src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryQueryImpl.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryQueryImpl.java
@@ -1,10 +1,20 @@
 package org.example.expert.domain.todo.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.comment.entity.QComment;
+import org.example.expert.domain.manager.entity.QManager;
+import org.example.expert.domain.todo.dto.response.QTodoSearchResponse;
+import org.example.expert.domain.todo.dto.response.TodoSearchResponse;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.user.entity.QUser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import org.example.expert.domain.todo.entity.QTodo;
 
@@ -17,6 +27,8 @@ public class TodoRepositoryQueryImpl implements TodoRepositoryQuery {
 
     private final QTodo todo = QTodo.todo;
     private final QUser user = QUser.user;
+    private final QManager manager = QManager.manager;
+    private final QComment comment = QComment.comment;
 
     @Override
     public Optional<Todo> findByIdWithUser(Long todoId) {
@@ -28,5 +40,78 @@ public class TodoRepositoryQueryImpl implements TodoRepositoryQuery {
                 .fetchOne();
 
         return Optional.ofNullable(result);
+    }
+
+    @Override
+    public Page<TodoSearchResponse> searchTodos(String title, LocalDateTime startDate, LocalDateTime endDate, String nickname, Pageable pageable) {
+
+        List<TodoSearchResponse> result = queryFactory
+                .select(new QTodoSearchResponse(
+                        todo.title,
+                        manager.id.countDistinct(),
+                        comment.id.countDistinct()
+                ))
+                .from(todo)
+                .where(
+                        titleContains(title),
+                        startDateGte(startDate),
+                        endDateLte(endDate),
+                        nicknameContains(user, nickname)
+                )
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .leftJoin(todo.managers, manager)
+                .leftJoin(manager.user, user)
+                .leftJoin(todo.comments, comment)
+                .groupBy(todo.id)
+                .orderBy(todo.modifiedAt.desc())
+                .fetch();
+
+        Long total = queryFactory
+                .select(todo.count())
+                .from(todo)
+                .where(
+                        titleContains(title),
+                        startDateGte(startDate),
+                        endDateLte(endDate)
+                ).fetchOne();
+
+        return new PageImpl<>(result, pageable, total);
+    }
+
+    private BooleanExpression titleContains(String title) {
+
+        if (title != null) {
+            return todo.title.contains(title);
+        }
+
+        return null;
+    }
+
+    private BooleanExpression startDateGte(LocalDateTime startDate) {
+
+        if (startDate != null) {
+            return todo.modifiedAt.goe(startDate);
+        }
+
+        return null;
+    }
+
+    private BooleanExpression endDateLte(LocalDateTime endDate) {
+
+        if (endDate != null) {
+            return todo.modifiedAt.loe(endDate);
+        }
+
+        return null;
+    }
+
+    private BooleanExpression nicknameContains(QUser user, String nickname) {
+
+        if (nickname != null) {
+            return user.nickname.contains(nickname);
+        }
+
+        return null;
     }
 }
